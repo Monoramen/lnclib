e8b9b4b3c9520ad4c17d8925350731f65f8c2cb1
Monoramen <monoramens@gmail.com>
2024-07-09 10:30:48 +0300

    Add Docs on readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 #### Пакет для создания БЦК (буквенно-цифрового кода) для слов
 
 SimpleReader - добавляет  в базу слова и соответствующие им БЦК
+    добавь этот код ниже для использования этих файлов словарей или используй свои из дирректории resources
+
+```java
+    // data/emo_dict.csv
+    // data/kartaslovsent.csv
+    // data/semantics.csv
+
+    System.out.println("\t========Начинаю создавать коды для слов.... =========\n".repeat(10));
+    String semanticsSimpleFile = "data/semantics.csv";
+    AbstractCSVReader reader = new SimpleReader();
+    reader.readDataLineByLine(semanticsSimpleFile);
+```
--- src/main/java/mnenobot/lnclib/ParseFile.java
+++ src/main/java/mnenobot/lnclib/ParseFile.java
@@ -6,16 +6,10 @@ import mnenobot.lnclib.db.SortCodeDataList;
 // CreateDict - читает данные из файла
 public class ParseFile {
     public static void main(String[] args) {
-       // String path = "/data/semantics_simple.csv";
-        DatabaseSetup.createTables();
-        //data/emo_dict.csv
-        ///data/kartaslovsent.csv
-        ///data/semantics.csv
-        //System.out.println("\t========Начинаю создавать коды для слов.... =========\n".repeat(10));
-        //String semanticsSimpleFile = "data/semantics.csv";
 
-        //AbstractCSVReader reader = new SimpleReader();
-        //reader.readDataLineByLine(semanticsSimpleFile);
+        DatabaseSetup.createTables();
+
+        
         SortCodeDataList codesList = new SortCodeDataList();
         System.out.println(codesList.getSortedData());
     }
